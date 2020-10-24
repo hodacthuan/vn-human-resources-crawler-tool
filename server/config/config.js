c5f8bf538c201588a0511f9fba3c7ca43333b50d
Thuan Ho <hodacthuan94@gmail.com>
2020-10-24 09:06:34 +0700

    Remove hardcode mongodb url

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
+/**
+ * Config globally
+ */
 const config = {
 	REMOVEJOB_DAYS: 30,
-	// MONGODB_URL: `mongodb://${process.env.CRAWL_USER}:${process.env.CRAWL_PASSWORD}@${process.env.SERVICE_NAME}-mongodb.${process.env.SERVICE_NAME}-network:27017/${process.env.CRAWL_NAME}`,
-	MONGODB_URL: `mongodb://crawl-user:crawl-password@mongodb.joco.asia:27018/crawl-database`,
+	MONGODB_URL: process.env.MONGODB_URL,
 	GOOGLE_CHROME_URL: '/usr/bin/google-chrome-stable',
 };
-// console.log(config.MONGODB_URL);
 
 module.exports = config;
